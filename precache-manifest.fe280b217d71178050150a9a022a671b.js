a3a6a310b732f3cb78cef5a7b08b5b0a8ff8960f
Zain Ul Abaidin <zainulabedin5566@gmail.com>
2020-08-14 01:07:36 +0500

    Updates

---
+++ precache-manifest.fe280b217d71178050150a9a022a671b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf359e7d9e2711f2d5349c9b21f033ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8aa093532694255576b7",
+    "url": "/static/css/main.5f05969b.chunk.css"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/static/js/2.8fa335fd.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/static/js/2.8fa335fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aa093532694255576b7",
+    "url": "/static/js/main.97df9bc7.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "d1065595f895b6c44878f5f9543df8e4",
+    "url": "/static/media/resume.d1065595.pdf"
+  }
+]);
